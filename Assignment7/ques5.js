b0ae7fd0519afeb2a4c31a492eccdb0683fb664b
sumitnegikv369 <sumit.negi@successive.tech>
2024-03-14 12:32:52 +0530

    ques5: runTask added to queue

--- Assignment7/ques5.js
+++ Assignment7/ques5.js
@@ -1,55 +1,55 @@
 // ? 5.Write a program to implement a Promise-based task queue, that processes tasks in a specified order, with a specified concurrency limit
 
 class TaskQueue {
-    constructor(concurrency) {
-        this.concurrency = concurrency;
-        this.runningTask = 0;
-        this.queue = [];
-    }
+  constructor(concurrency) {
+    this.concurrency = concurrency;
+    this.runningTask = 0;
+    this.queue = [];
+  }
 
-    runTask(task) {
-        return new Promise((resolve, reject) => {
-            const createdTask = async () => {
-                this.runningTask++;
-                try {
-                    const result = await task();
-                    resolve(result);
-                } catch (error) {
-                    reject(error);
-                } finally {
-                    this.runningTask--;
-                    this.processQueue();
-                }
+  runTask(task) {
+    if (this.runningTask < this.concurrency) {
+      this.runningTask++;
+      this.queue.push(
+        new Promise((resolve, reject) => {
+          const createdTask = async () => {
+            try {
+              const result = await task();
+              resolve(result);
+            } catch (error) {
+              reject(error);
             }
-
-            if (this.runningTask < this.concurrency) {
-                createdTask();
-            } else {
-                this.queue.push(createdTask);
-            }
-        });
+          };
+          createdTask();
+        })
+      );
+    } else {
+      this.runningTask--;
+      this.processQueue();
     }
-    processQueue() {
-        if (this.queue.length > 0 && this.runningTask < this.concurrency) {
-            const task = this.queue.shift();
-            task();
-        }
+  }
+  processQueue() {
+    if (this.queue.length > 0) {
+      this.queue.shift();
     }
+  }
 }
 
 const tq = new TaskQueue(2);
 
-const delayed = () => new Promise((resolve) => {
+const delayed = () =>
+  new Promise((resolve) => {
     setTimeout(() => {
-        resolve("Task completed");
+      resolve("Task completed");
     }, 1000);
-})
+  });
 
 for (let i = 0; i < 10; i++) {
-    let taskNumber = i;
-    tq.runTask(async () => {
-        console.log(`starting task number ${taskNumber}`);
-        await delayed();
-        console.log(`ending task number ${taskNumber}`);
-    })
+  let taskNumber = i;
+  tq.runTask(async () => {
+    console.log(`starting task number ${taskNumber}`);
+    const response = await delayed();
+    console.log(response);
+    console.log(`ending task number ${taskNumber}`);
+  });
 }
